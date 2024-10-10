4ce350eca9a384ccded4603649e04162eaae0f50
제섭test <>
2024-10-10 17:12:49 +0900

    파일양식 및 컨트롤러 서비스 합치고 사이드바 mes_ 붙여서 수정

--- mes_page/src/main/java/project3/gamja/mesPage/dao/BookflixDAO.java
+++ mes_page/src/main/java/project3/gamja/mesPage/dao/BookflixDAO.java
@@ -11,7 +11,7 @@ import project3.gamja.mesPage.dto.MesReviewDTO;
 
 
 @Mapper
-public interface BuserDAO {
+public interface BookflixDAO {
     
     // 특정 user_id2로 사용자 조회
     List<MesBuserDTO> selectBuser(@Param("user_id2") String user_id2);
