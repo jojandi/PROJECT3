b9ab2267ae9fe5b9829a02be62ddd48362fa9174
조민정 <mg518220@gmail.com>
2024-10-10 16:58:12 +0900

    프로젝트 새로 만듦, admin page -> 실시간 재고관리 완료, 예약 관리 ing

--- admin_page3/src/main/java/project3/gamja/admin/dao/InvenDAO.java
+++ admin_page3/src/main/java/project3/gamja/admin/dao/InvenDAO.java
@@ -5,11 +5,18 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import project3.gamja.admin.dto.InvenDTO;
+import project3.gamja.admin.dto.LoanResDTO;
 
 @Mapper
 public interface InvenDAO {
 
 	List<InvenDTO> selectInven(InvenDTO invenDTO);
-	int totalInven();
+	int totalInven(InvenDTO invenDTO);
+	List<InvenDTO> selectRes(LoanResDTO loanResDTO);
+	int totalRes(LoanResDTO loanResDTO);
+	int updateRes(LoanResDTO loanResDTO);
+	int inseResLoan(LoanResDTO loanResDTO);
+	int invenLoan(LoanResDTO loanResDTO);
+	int invenRes(LoanResDTO loanResDTO);
 	
 }
