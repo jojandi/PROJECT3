a2cebc644901e503cf3f594e9d1f79ec40c78b43
DESKTOP-9V6G0CO\user <dkdud532@naver.com>
2024-10-12 15:27:23 +0900

    no message

--- mes_page/src/main/java/project3/gamja/mesPage/controller/HumanController.java
+++ mes_page/src/main/java/project3/gamja/mesPage/controller/HumanController.java
@@ -4,84 +4,74 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import project3.gamja.mesPage.dto.MesHumanDTO;
-import project3.gamja.mesPage.dto.MesNoticeDTO;
 import project3.gamja.mesPage.service.HumanService;
 
 @Controller
 public class HumanController {
 
-	@Autowired
-	HumanService humanService;
+    @Autowired
+    HumanService humanService;
 
-	@RequestMapping("/mes_human")
-	public String human(Model model) {
-		List<MesHumanDTO> list = humanService.selectHuman();
-		model.addAttribute("list", list);
+    @RequestMapping("/mes_human")
+    public String human(Model model) {
+        List<MesHumanDTO> list = humanService.selectHuman();
+        model.addAttribute("list", list);
 
-		return "mes_human";
-	}
-	
-	/*
-	 * @RequestMapping("/mes_read") public String human2(Model model) {
-	 * List<MesHumanDTO> list = humanService.selectHuman();
-	 * model.addAttribute("humanlist", list);
-	 * 
-	 * return "mes_read"; }
-	 */
+        return "mes_human";
+    }
 
-	
-	@RequestMapping("/mes_read")
-	public String readNotice (@RequestParam int emp_id, Model model) {
-		System.out.println("read 실행확인");
-		MesHumanDTO dto = new MesHumanDTO();
-		dto.setEmp_id(emp_id);
-		
-		MesHumanDTO list = humanService.selectOne(dto);
-		model.addAttribute("dto", list);
+    @RequestMapping("/mes_read")
+    public String readNotice(@RequestParam int emp_id, Model model) {
+        System.out.println("read 실행확인");
+        MesHumanDTO dto = new MesHumanDTO();
+        dto.setEmp_id(emp_id);
 
-		
-		return "mes_read";
-	}
-	
-	@RequestMapping("/mes_humanUpdate")
-	public String updateHuman2 (@RequestParam int emp_id, Model model) {
-		
-		MesHumanDTO dto = new MesHumanDTO();
-		dto.setEmp_id(emp_id);
-		
-		MesHumanDTO list = humanService.selectOne(dto);
-		model.addAttribute("updateHuman2", list);
-		
-		
-		return "mes_humanUpdate";
-	}
-	
-	@RequestMapping(value="/mes_humanDelete", method=RequestMethod.GET)
-	public String deleteHuman(MesHumanDTO dto, @RequestParam Integer emp_id) {
-	    
-	    try {
-	        humanService.deleteHuman(dto);  
-	    } catch (Exception e) {
-	        System.out.println(e);
-	    }
-	    
-	    return "redirect:/mes_humanDelete";  
-	}
+        MesHumanDTO list = humanService.selectOne(dto);
+        model.addAttribute("dto", list);
 
-	
+        return "mes_read";
+    }
 
-	
+    @RequestMapping("/mes_humanUpdate")
+    public String updateHuman(@RequestParam int emp_id, Model model) {
 
-	
+        MesHumanDTO dto = new MesHumanDTO();
+        dto.setEmp_id(emp_id);
 
+        MesHumanDTO humanData = humanService.selectOne(dto);
+        model.addAttribute("updateHuman", humanData);
 
-	
-		
+        return "mes_humanUpdate";
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+    public String modifyHuman(MesHumanDTO dto) {
+        try {
+            humanService.updateHuman(dto); // 데이터 수정
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/mes_human"; // 수정 후 목록 페이지로 이동
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public String deleteHuman(@RequestParam int emp_id) {
+        MesHumanDTO dto = new MesHumanDTO();
+        dto.setEmp_id(emp_id);
+
+        try {
+            humanService.deleteHuman(dto); // 데이터 삭제
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("삭제확인");
+        return "redirect:/mes_human"; // 삭제 후 목록 페이지로 이동
+    }
 }
+
