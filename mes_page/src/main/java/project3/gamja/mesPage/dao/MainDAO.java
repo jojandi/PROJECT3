c7fb9d2f15f0b5d7ddb241ba27b38d7a8628f81d
제섭test <>
2024-10-18 09:27:26 +0900

    메인페이지 공지사항 출력

--- mes_page/src/main/java/project3/gamja/mesPage/dao/MainDAO.java
+++ mes_page/src/main/java/project3/gamja/mesPage/dao/MainDAO.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import project3.gamja.mesPage.dto.MesMainDTO;
+import project3.gamja.mesPage.dto.MesNoticeDTO;
 
 @Mapper
 public interface MainDAO {
 	
 	List<MesMainDTO> selectAll();
 	 List<MesMainDTO> getStatisticsByGenre(int year, int month);
+	  List<MesMainDTO> findNoticesByEmpId(int emp_id);
 
 }
